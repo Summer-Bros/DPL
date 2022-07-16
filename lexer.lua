a317db3f7641c550711531a490925bdacaca63a4
Luanism <91706547+Luanism@users.noreply.github.com>
2022-07-16 17:33:21 -0500

    added stuff to lexer.lua

--- lexer.lua
+++ lexer.lua
@@ -1,3 +1,20 @@
 local function lex(contents)
-  -- do stuff
+    local words = {}
+    do
+        local word = ""
+        local char = ""
+        for pos = 1, contents:len() do
+            char = contents:sub(pos, pos)
+            if char == " " then
+                table.insert(words, word)
+                word = ""
+            else
+                word = word .. char
+            end
+        end
+        table.insert(words, word)
+    end
+    return words
 end
+
+return lex
