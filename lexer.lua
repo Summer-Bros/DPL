4ad7cf26ec347c7bf11230d730c5502dfef97230
Luanism <91706547+Luanism@users.noreply.github.com>
2022-07-17 16:44:41 -0500

    fixes for lexer.lua

--- lexer.lua
+++ lexer.lua
@@ -5,7 +5,7 @@ local function lex(contents)
         local char = ""
         for pos = 1, contents:len() do
             char = contents:sub(pos, pos)
-            if char == " " then
+            if (char == " ") or (char == "\n") then
                 table.insert(words, word)
                 word = ""
             else
